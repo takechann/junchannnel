76eaf9bb3d0783075979e9e41f8ae254c6fb6a0e
Takechann <b1014154@fun.ac.jp>
2017-11-16 17:49:09 +0900

    xcode作成

---
+++ junchannnel/junchannnel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  junchannnel
+//
+//  Created by  Takeya Kurose on 2017/11/16.
+//  Copyright © 2017年  Takeya Kurose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
